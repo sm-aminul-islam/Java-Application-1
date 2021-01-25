c3886045c8e112a350eaf1efa6ba16bfab2c2d67
devil <devil@DESKTOP-QBITRB3>
2021-01-25 13:22:53 +0600

    This is a Addition project

--- src/sum/Sum.java
+++ src/sum/Sum.java
@@ -1,21 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package sum;
 
-/**
- *
- * @author devil
- */
-public class Sum {
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
+
+public class Sum {
+    public static void test(){
+         Scanner sc = new Scanner (System.in);
+        System.out.println("Enter two number:");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int result = x+y;
+        System.out.printf("addition result : %d\n",result);
+
+}
     public static void main(String[] args) {
-        // TODO code application logic here
+        test();
+       
+       
     }
     
 }
